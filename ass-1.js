92cd235467121227b89166568d7fbf025db875e5
fikante <154019349+fikante@users.noreply.github.com>
2024-07-17 18:26:28 +0300

    assignment-1

---
+++ ass-1.js
@@ -0,0 +1,18 @@
+//Exercise 1
+    // function starPattern(){
+    //     for(i=0; i<5; i++){
+    //         console.log("*".repeat(i+1));
+    //     }
+    // }
+    // starPattern();
+//Exercise 2
+    function GCF(num1,num2){
+        while(num2!=0){
+           let r = num1 % num2;
+            num1=num2;
+            num2=r;
+        }
+        return num1
+    }
+    let result = GCF(48,18)
+    console.log(result)
