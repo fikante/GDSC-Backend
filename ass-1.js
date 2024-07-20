5b850d85ea8e075232dfcf32fd1404a652686bdc
fikante <154019349+fikante@users.noreply.github.com>
2024-07-20 16:29:27 +0300

    Update ass-1.js

--- ass-1.js
+++ ass-1.js
@@ -6,25 +6,15 @@
     }
     starPattern();
 //Exercise 2
-    // function GCF(num1,num2){
-    //     while(num2!=0){
-    //        let r = num1 % num2;
-    //         num1=num2;
-    //         num2=r;
-    //     }
-    //     return num1
-    // }
-    // let result = GCF(48,18)
-    // console.log(result)
-
-// Pop quiz 2.1
-    function factorial(n){
-        if(n==0 || n==1){
-            return 1
+    function GCF(num1,num2){
+        while(num2!=0){
+           let r = num1 % num2;
+            num1=num2;
+            num2=r;
         }
-        else{
-            return n * factorial(n-1);
-        }
+        return num1
     }
-    let fac = factorial(5)
-    console.log(fac)
+    let result = GCF(48,18)
+    console.log(result)
+
+
